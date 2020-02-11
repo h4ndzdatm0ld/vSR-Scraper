19d2c995114eb98f3093fb661973713a40cf66a4
Hugo Tinoco <hugotinoco@icloud.com>
2020-02-11 15:20:32 -0800

    Phase 1

--- vSR-Scraper/vSR_Scraper.py
+++ vSR-Scraper/vSR_Scraper.py
@@ -1,1 +1,12 @@
+import re
 
+admin_pw = re.compile(r'(password).+')
+
+#Validate encoding if necessary or not. 
+with open ('config.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+
+    passwords = admin_pw.finditer(contents)
+
+    for match in passwords:
+        print(match)

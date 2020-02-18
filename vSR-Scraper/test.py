c4bc3f18d9e4d1010504dab9bb377b94b58829c3
Hugo Tinoco <hugotinoco@icloud.com>
2020-02-17 16:01:39 -0800

    Beta 1

--- vSR-Scraper/test.py
+++ vSR-Scraper/test.py
@@ -1,20 +1,41 @@
+# import re
+# admin_pw = re.compile('^.*password.+$', re.MULTILINE)
+#
 import re
-admin_pw = re.compile('^.*password.+$', re.MULTILINE)
-
+exit_all = re.compile('Finished.*', re.MULTILINE | re.DOTALL)
 def main():
- with open ('config.txt', 'r+') as f:
-    scrubbed = []
-    for line in f.readlines():
-        if re.match(admin_pw, line):
-            continue
-        scrubbed.append(line)
-    print("".join(scrubbed))
+ with open ('SPFDILUP91A-P-AL-7750-01.txt', 'r+', encoding='utf-8') as f:
+     contents = f.read()
+     EOLine = exit_all.finditer(contents)
+     for match in EOLine:
+        print(match)
+     Scrubbed = re.sub(exit_all, '', contents, re.MULTILINE)
+     # print(Scrubbed)
+
+
 if __name__ == "__main__": main()
-
-
-
-
-    # contents = f.read()
-    # passwords = admin_pw.finditer(contents)
-    # for match in passwords:
-    #  print(match)
+#
+#
+#     # contents = f.read()
+#     # passwords = admin_pw.finditer(contents)
+#     # for match in passwords:
+# #     #  print(match)
+# import re
+#
+# regex = r"# Finished.*"
+# file="SPFDILUP91A-P-AL-7750-01.txt"
+#
+#
+# with open (file, 'r+', encoding='utf-8') as f:
+# 	contents = f.read()
+# 	matches = re.finditer(regex, contents, re.MULTILINE | re.DOTALL)
+#
+# 	#
+# 	# for matchNum, match in enumerate(matches, start=1):
+# 	#
+# 	#     print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+# 	#
+# 	#     for groupNum in range(0, len(match.groups())):
+# 	#         groupNum = groupNum + 1
+# 	#
+# 	#         print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
--- vSR-Scraper/vSR_Scraper.py
+++ vSR-Scraper/vSR_Scraper.py
@@ -1,28 +1,93 @@
------------------------------------------------------------
-# Created by Hugo Tinoco :: hugo.tinoco.ext@nokia.com
-# Version 0.1 (Beta) - 2/12/2020
 #!/usr/bin/python3
------------------------------------------------------------
 
+#############################################################
+# Author: Hugo Tinoco :: hugo.tinoco.ext@nokia.com          #
+# Version 0.1 (Beta) - 2/12/2020                            #
+#                                                           #
+#############################################################
+import time
 import re
 
 # Regex utilized to scrub the configuration files
 
-admin_pw = re.compile('^.*password.*$', re.MULTILINE)
-netc = re.compile('^.*netconf.+$', re.MULTILINE)
+ADMIN_PW = re.compile('^.*password.*$', flags=re.MULTILINE)
+DISP_CONF = re.compile('^.*admin di.+$', flags=re.MULTILINE)
+SOT_LINES = re.compile('^.*Generated\s\w+\s\w+\s+\d+\s\d+.\d+.\d+\s\d+\s\w+', re.DOTALL )
+EOT_LINES = re.compile('Finished.*', re.MULTILINE | re.DOTALL)
+MGMT_RTZ = re.compile('^.*static\D\w+\D\w+\s\d+.\d+.\d+.\d+\D\d+\s+next-hop\s\d+.\d+.\d+.\d+\s+exit\s+exit', re.MULTILINE)
+
+# Replace the file name with the new configuration file being scrubbed.
+file="SPFDILUP91A-P-AL-7750-01.txt"
+
+# The first function runs the regex searches line by line, in order to
+# eliminate syntax + empty lines within the configuration file that are
+# left beind.
+
+def ALL_Lines():
+    with open (file, 'r+', encoding='utf-8') as f:
+        scrape = []
+        for line in f.readlines():
+            if re.match(ADMIN_PW, line):
+                print(line)
+                continue
+            if re.match(DISP_CONF, line):
+                print(line)
+                continue
+            scrape.append(line)
+        scrubbed = ("".join(scrape))
+        x = open('scrubbed-pre.txt', 'w')
+        x.write(scrubbed)
+        x.close()
+        time.sleep(5) # Some config files are quite large, assert enough time.
+
+# The second function opens the scrubbed file and elimnates the unennsacrry syntax
+# at the begining and end of the configuration syntax. This ensures the syntax
+# doesn't fail when importing to the vSR.
+
+def SO_Text():
+    file = "scrubbed-pre.txt"
+    with open (file, 'r+', encoding='utf-8') as f:
+        contents = f.read()
+        FirstLines = SOT_LINES.finditer(contents)
+        for match in FirstLines:
+            print(match)
+        yz = re.sub(SOT_LINES, '', contents)
+        x = open('scrubbed-pre.txt', 'w')
+        x.write(yz)
+        x.close()
+        time.sleep(2) # Some config files are quite large, assert enough time.
+
+def EO_Text():
+    file = "scrubbed-pre.txt"
+    with open (file, 'r+', encoding='utf-8') as f:
+        contents = f.read()
+        LastLines = EOT_LINES.finditer(contents)
+        for match in LastLines:
+            print(match)
+        yx = re.sub(EOT_LINES, '', contents)
+        x = open('scrubbed-pre.txt', 'w')
+        x.write(yx)
+        x.close()
+        time.sleep(2) # Some config files are quite large, assert enough time.
+
+def MGMT_Routes():
+    file = "scrubbed-pre.txt"
+    with open (file, 'r+', encoding='utf-8') as f:
+        contents = f.read()
+        Routes = MGMT_RTZ.finditer(contents)
+        for match in Routes:
+            print(match)
+        yi = re.sub(MGMT_RTZ, '#', contents)
+        x = open('scrubbed.txt', 'w')
+        x.write(yi)
+        x.close()
+        time.sleep(1) # Some config files are quite large, assert enough time.
 
 def main():
+    ALL_Lines()
+    SO_Text()
+    EO_Text()
+    MGMT_Routes()
 
- with open ('config.txt', 'r+', encoding='utf-8') as f:
-    scrape = []
-    for line in f.readlines():
-     if re.match(admin_pw, line):
-         continue
-     if re.match(netc, line):
-         continue
-     scrape.append(line)
-
-    scrubbed = ("".join(scrape))
-    print(scrubbed)
 
 if __name__ == "__main__": main()
